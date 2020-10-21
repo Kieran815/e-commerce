10e0c91747ac285b49938cce6f0020a2a7256396
Kieran815 <kieran.milligan@gmail.com>
2020-10-21 14:11:46 -0500

    Updates

---
+++ precache-manifest.94ae43b72ad3dc3653111ddfe95442a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcc0d570693fdb80642f48f5c736815",
+    "url": "/shopping_cart_basic/index.html"
+  },
+  {
+    "revision": "5619bccb3c9dc9edca3c",
+    "url": "/shopping_cart_basic/static/css/2.3807f13a.chunk.css"
+  },
+  {
+    "revision": "121ae31b84dbb9ce1420",
+    "url": "/shopping_cart_basic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5619bccb3c9dc9edca3c",
+    "url": "/shopping_cart_basic/static/js/2.00c6746e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping_cart_basic/static/js/2.00c6746e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "121ae31b84dbb9ce1420",
+    "url": "/shopping_cart_basic/static/js/main.fd2fc568.chunk.js"
+  },
+  {
+    "revision": "f1baa4529ef19383480f",
+    "url": "/shopping_cart_basic/static/js/runtime-main.52c97c28.js"
+  }
+]);
